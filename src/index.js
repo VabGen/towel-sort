a3f4e5f4874d7add1e2b90d6ca9c6aeea02a4b91
VabGen <vabgen@gmail.com>
2022-08-11 21:44:45 +0300

    fix: declaration of constants in a file index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix){
+	let asc = true
+	let result = [];
+	for(let i=0; i<matrix.length; i++){
+		for(let j=0; j<matrix[i].length; j++){
+			if (asc){
+				result.push(matrix[i][j])
+			}else{
+				result.push(matrix[i][matrix[i].length-1-j]);
+			}
+		}
+		asc=!asc;
+	}
+  return result
 }
