6d98d712958259a2b340646ba390f654ed86716c
VabGen <vabgen@gmail.com>
2022-08-11 21:49:55 +0300

    fix: declaration of constants in a file index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-module.exports = function towelSort(matrix){
-	let asc = true
-	let result = [];
-	for(let i=0; i<matrix.length; i++){
-		for(let j=0; j<matrix[i].length; j++){
-			if (asc){
-				result.push(matrix[i][j])
-			}else{
-				result.push(matrix[i][matrix[i].length-1-j]);
-			}
-		}
-		asc=!asc;
-	}
+module.exports = function towelSort(matrix) {
+  if (matrix.length == 0) {
+    return []
+  }
+  let asc = true
+  let result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (asc) {
+        result.push(matrix[i][j])
+      } else {
+        result.push(matrix[i][matrix[i].length - 1 - j]);
+      }
+    }
+    asc = !asc;
+  }
   return result
 }
